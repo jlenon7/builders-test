a9877d80789bad500847f14e4b2c70fc2c43d58a
João Lenon <lenonsec7@gmail.com>
2021-08-23 14:13:48 -0300

    chore: Change exception name, remove unused imports

--- src/main/java/br/com/sec/config/WebConfig.java
+++ src/main/java/br/com/sec/config/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
--- src/main/java/br/com/sec/exception/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/br/com/sec/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -1,8 +1,8 @@
 package br.com.sec.exception.handler;
 
+import br.com.sec.exception.BadRequestException;
 import br.com.sec.exception.ExceptionResponse;
 import br.com.sec.exception.NotFoundException;
-import br.com.sec.exception.UnsupportedMathOperationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -27,7 +27,7 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(UnsupportedMathOperationException.class)
+    @ExceptionHandler(BadRequestException.class)
     public final ResponseEntity<ExceptionResponse> handleBadRequestException(Exception ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(
                 new Date(),
